dfd60dd2353ac572a87817ddaf512df0deeab56e
andrewnjoguu <andrew.njogu@student.moringaschool.com>
2021-10-12 11:43:26 +0300

    custom class created

---
+++ src/app/form.model.ts
@@ -0,0 +1,19 @@
+export class Form{
+    name: string
+    quote: string
+    vote: number
+
+    constructor(name: string,quote: string, vote?:number){
+ this.name =name
+ this.quote = quote
+this.vote = vote || 0
+    }
+
+    voteUp(){
+        this.vote += 1
+    }
+
+    voteDown(){
+        this.vote -= 1
+    }
+}
