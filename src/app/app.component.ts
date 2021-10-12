d0e46c70e4a32ff02dfce540225cac0db6a52590
andrewnjoguu <andrew.njogu@student.moringaschool.com>
2021-10-12 11:40:15 +0300

    added ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Form } from './form.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'best-quote';
+  form: Form[]
+
+  constructor() {
+    this.form =  [
+      new Form('Andrew', 'nothing comes easy', 3),
+      new Form('Morio wa South', 'being nice costs you nothing', 2),
+      new Form('Amy ', 'love is not a one way street', 1),
+    ]
+  }
+
+  
+  add_quote(name: HTMLInputElement, quote: HTMLInputElement){
+   this.form.push(new Form(name.value, quote.value))
+   name.value = ''
+   quote.value = ''
+  }
 }
